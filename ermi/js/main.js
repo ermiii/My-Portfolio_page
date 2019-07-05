1798e6e6ccdcf07ad8d8d716fae08e9cbc30af7c
Ermiyas Wondimu <vectonv@gmail.com>
2019-07-05 16:11:09 +0100

    final portfolio

--- ermi/js/main.js
+++ ermi/js/main.js
@@ -26,3 +26,20 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
